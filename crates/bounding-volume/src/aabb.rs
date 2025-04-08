0e83b201b81cb197b6b1a74f683b1a861220bd68
Antoine Groudiev <antoine.groudiev@ens.psl.eu>
2025-04-07 21:12:50 -0400

    feat(bv): added a `BoundingVolume` trait

--- crates/bounding-volume/src/aabb.rs
+++ crates/bounding-volume/src/aabb.rs
@@ -1,5 +1,7 @@
 use nalgebra::Point3;
 
+use crate::BoundingVolume;
+
 /// Axis-Aligned Bounding Box (AABB) structure.
 /// An AABB is defined by its minimum and maximum corners in 3D space.
 #[derive(PartialEq, Debug, Clone, Copy)]
@@ -19,5 +21,38 @@ impl AABB {
     /// * `max` - The maximum corner of the AABB.
     pub fn new(min: Point3<f32>, max: Point3<f32>) -> Self {
         AABB { min, max }
+    }
+}
+
+impl BoundingVolume for AABB {
+    fn center(&self) -> Point3<f32> {
+        nalgebra::center(&self.min, &self.max)
+    }
+
+    fn contains_point(&self, point: &nalgebra::Point3<f32>) -> bool {
+        point.x >= self.min.x
+            && point.x <= self.max.x
+            && point.y >= self.min.y
+            && point.y <= self.max.y
+            && point.z >= self.min.z
+            && point.z <= self.max.z
+    }
+
+    fn contains_volume(&self, other: &Self) -> bool {
+        self.min.x <= other.min.x
+            && self.max.x >= other.max.x
+            && self.min.y <= other.min.y
+            && self.max.y >= other.max.y
+            && self.min.z <= other.min.z
+            && self.max.z >= other.max.z
+    }
+
+    fn intersects(&self, other: &Self) -> bool {
+        self.min.x <= other.max.x
+            && self.max.x >= other.min.x
+            && self.min.y <= other.max.y
+            && self.max.y >= other.min.y
+            && self.min.z <= other.max.z
+            && self.max.z >= other.min.z
     }
 }
--- crates/bounding-volume/src/bounding_sphere.rs
+++ crates/bounding-volume/src/bounding_sphere.rs
@@ -1,5 +1,7 @@
 use nalgebra::Point3;
 
+use crate::BoundingVolume;
+
 /// A bounding sphere.
 #[derive(PartialEq, Debug, Clone, Copy)]
 pub struct BoundingSphere {
@@ -18,5 +20,25 @@ impl BoundingSphere {
     /// * `radius` - The radius of the sphere.
     pub fn new(center: Point3<f32>, radius: f32) -> Self {
         BoundingSphere { center, radius }
+    }
+}
+
+impl BoundingVolume for BoundingSphere {
+    fn center(&self) -> Point3<f32> {
+        self.center
+    }
+
+    fn contains_point(&self, point: &Point3<f32>) -> bool {
+        nalgebra::distance_squared(&self.center, point) <= self.radius * self.radius
+    }
+
+    fn contains_volume(&self, other: &Self) -> bool {
+        nalgebra::distance_squared(&self.center, &other.center) + other.radius * other.radius
+            <= self.radius * self.radius
+    }
+
+    fn intersects(&self, other: &Self) -> bool {
+        nalgebra::distance_squared(&self.center, &other.center)
+            <= (self.radius + other.radius) * (self.radius + other.radius)
     }
 }
