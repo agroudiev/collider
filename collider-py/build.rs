6de5700010c1be48501ff7d4b7731b133020b5e2
Antoine Groudiev <antoine.groudiev@ens.psl.eu>
2026-01-24 17:45:21 +0100

    chore: improve build script

--- collider-py/build.rs
+++ collider-py/build.rs
@@ -27,6 +27,41 @@ pub fn main() {
         eprintln!("stderr: {}", String::from_utf8_lossy(&output.stderr));
         panic!("Python command failed");
     }
-    let python_lib_path = String::from_utf8_lossy(&output.stdout);
-    println!("cargo:rustc-link-arg=-Wl,-rpath,{}", python_lib_path);
+
+    // Get LIBDIR
+    let python_libdir = {
+        let output = Command::new("python")
+            .arg("-c")
+            .arg("from distutils import sysconfig; print(sysconfig.get_config_var('LIBDIR'))")
+            .output()
+            .expect("Failed to get LIBDIR");
+        if !output.status.success() {
+            panic!("Failed to get LIBDIR");
+        }
+        String::from_utf8_lossy(&output.stdout).trim().to_string()
+    };
+
+    // Get LIBRARY (e.g., python3.11)
+    let python_lib = {
+        let output = Command::new("python")
+            .arg("-c")
+            .arg("from distutils import sysconfig; print(sysconfig.get_config_var('LDLIBRARY'))")
+            .output()
+            .expect("Failed to get LDLIBRARY");
+        if !output.status.success() {
+            panic!("Failed to get LDLIBRARY");
+        }
+        String::from_utf8_lossy(&output.stdout).trim().to_string()
+    };
+
+    // Remove 'lib' prefix and '.dylib' or '.a' suffix to get the correct name for rustc-link-lib
+    let libname = python_lib
+        .strip_prefix("lib")
+        .unwrap_or(&python_lib)
+        .trim_end_matches(".dylib")
+        .trim_end_matches(".a");
+
+    println!("cargo:rustc-link-search=native={}", python_libdir);
+    println!("cargo:rustc-link-lib=dylib={}", libname);
+    println!("cargo:rustc-link-arg=-Wl,-rpath,{}", python_libdir);
 }
