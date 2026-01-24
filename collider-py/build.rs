35096cce5910dbb379c732df980d6f84746c3a7c
Antoine Groudiev <antoine.groudiev@ens.psl.eu>
2026-01-24 17:59:06 +0100

    fix(CI): update build script

--- collider-py/build.rs
+++ collider-py/build.rs
@@ -1,31 +1,12 @@
 use std::process::Command;
-pub fn main() {
-    // Install setuptools
-    let output = Command::new("pip")
-        .arg("install")
-        .arg("setuptools")
-        .output()
-        .expect("Failed to execute command");
-    if !output.status.success() {
-        eprintln!("Command failed with status: {}", output.status);
-        eprintln!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-        eprintln!("stderr: {}", String::from_utf8_lossy(&output.stderr));
-        panic!("Installation of setuptools failed");
-    }
 
-    // Run distutils to get the library path
-    let python_inline_script =
-        "from distutils import sysconfig;print(sysconfig.get_config_var('LIBDIR'))";
-    let output = Command::new("python")
-        .arg("-c")
-        .arg(python_inline_script)
-        .output()
-        .expect("Failed to get LIBDIR");
-    if !output.status.success() {
-        eprintln!("Python command failed with status: {}", output.status);
-        eprintln!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-        eprintln!("stderr: {}", String::from_utf8_lossy(&output.stderr));
-        panic!("Python command failed");
+pub fn main() {
+    // Skip this build script when building documentation on docs.rs or when in CI
+    if std::env::var("DOCS_RS").is_ok()
+        || std::env::var("CI").is_ok()
+        || std::env::var("GITHUB_ACTIONS").is_ok()
+    {
+        return;
     }
 
     // Get LIBDIR
@@ -61,7 +42,7 @@ pub fn main() {
         .trim_end_matches(".dylib")
         .trim_end_matches(".a");
 
-    println!("cargo:rustc-link-search=native={}", python_libdir);
-    println!("cargo:rustc-link-lib=dylib={}", libname);
-    println!("cargo:rustc-link-arg=-Wl,-rpath,{}", python_libdir);
+    println!("cargo:rustc-link-search=native={python_libdir}");
+    println!("cargo:rustc-link-lib=dylib={libname}");
+    println!("cargo:rustc-link-arg=-Wl,-rpath,{python_libdir}");
 }
